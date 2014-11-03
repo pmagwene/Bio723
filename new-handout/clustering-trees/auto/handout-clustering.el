b07bfcb2550ef4a46b1ca411ce6e89e93d0f0424
Paul Magwene <paul.magwene@duke.edu>
2014-11-03 17:30:05 -0500

    Working on updated clustering handout

---
+++ new-handout/clustering-trees/auto/handout-clustering.el
@@ -0,0 +1,32 @@
+(TeX-add-style-hook
+ "handout-clustering"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "minted"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec-1"
+    "sec-1-1"
+    "sec-1-2")))
+
