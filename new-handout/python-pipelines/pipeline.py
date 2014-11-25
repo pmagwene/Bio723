9105f6630df0c34fd8cd24ae867b41cedea00a23
Paul Magwene <paul.magwene@duke.edu>
2014-11-24 20:41:51 -0500

    Updated hands-on 13

--- new-handout/python-pipelines/pipeline.py
+++ new-handout/python-pipelines/pipeline.py
@@ -1,13 +1,13 @@
 """
 pipline.py --  An illustrative example of a bioinformatics pipeline.
 Requires Python 2.6+ and BioPython 1.53+
-(c) Copyright by Paul M. Magwene, 2009-2011  (mailto:paul.magwene@duke.edu)
+(c) Copyright by Paul M. Magwene, 2009-2014  (mailto:paul.magwene@duke.edu)
 """
 
 from Bio import Seq, SeqIO, SeqRecord
 from Bio import ExPASy, SwissProt
 from Bio.Blast import NCBIWWW, NCBIXML
-    
+
 import glob, subprocess
 
 def read_fasta(infile):
@@ -18,27 +18,26 @@ def read_fasta(infile):
 def parse_fasta(infile):
     """Read multiple sequences from a FASTA file"""
     recs = SeqIO.parse(infile,'fasta')
-    return [i for i in recs] 
-    
+    return [i for i in recs]
+
 def write_fasta(recs, outfile):
-    ofile = open(outfile, 'w')
-    SeqIO.write(recs, ofile, 'fasta')    
-    
+    SeqIO.write(recs, outfile, 'fasta')
+
 def translate_recs(seqrecs):
-    """ nucleotide SeqRecords -> translated protein SeqRecords """ 
+    """ nucleotide SeqRecords -> translated protein SeqRecords """
     proteins = []
     for rec in seqrecs:
         aaseq = rec.seq.translate()
-        protrec = SeqRecord.SeqRecord(aaseq, id=rec.id, name=rec.name, 
+        protrec = SeqRecord.SeqRecord(aaseq, id=rec.id, name=rec.name,
         			      description=rec.description)
         proteins.append(protrec)
-    return proteins    
-    
+    return proteins
+
 def translate_fasta(infile, outfile):
     """ nucleotide fasta file -> protein fasta file """
     nrecs = parse_fasta(infile)
     precs = translate_recs(nrecs)
-    write_fasta(precs, outfile)    
+    write_fasta(precs, outfile)
 
 def inout_pairs(insuffix, outsuffix):
     """ Files in directory with given suffix -> list of tuples w/ (infile,outfile)"""
@@ -48,17 +47,17 @@ def inout_pairs(insuffix, outsuffix):
         inprefix = infile[:-len(insuffix)]
         outfile = inprefix + outsuffix
         pairs.append((infile,outfile))
-    return pairs     
-    
+    return pairs
+
 def blastp(seqrec, outfile, database='nr', entrez_query='(none)'):
-    handle = NCBIWWW.qblast('blastp', database, seqrec.seq.tostring(), 
+    handle = NCBIWWW.qblast('blastp', database, seqrec.seq.tostring(),
     				entrez_query=entrez_query)
     results = handle.read()
     sfile = open(outfile, 'w')
     sfile.write(results)
-    sfile.close()   
+    sfile.close()
     bout = open(outfile, 'r')
-    brecord = NCBIXML.read(bout)    
+    brecord = NCBIXML.read(bout)
     return brecord
 
 def summarize_blastoutput(brecord):
@@ -68,47 +67,47 @@ def summarize_blastoutput(brecord):
         accession = alignment.accession
         hits.append((expect,accession))
     hits.sort() # will sort tuples by their first value (i.e. expect)
-    return hits       
+    return hits
 
 def get_swissrec(accession):
     handle = ExPASy.get_sprot_raw(accession)
     record = SwissProt.read(handle)
     return record
-    
+
 def swissrec2seqrec(record):
     seq = Seq.Seq(record.sequence, Seq.IUPAC.protein)
-    s = SeqRecord.SeqRecord(seq, description=record.description, 
+    s = SeqRecord.SeqRecord(seq, description=record.description,
                 id=record.accessions[0], name=record.entry_name)
-    return s             
+    return s
 
 def mafft_align(infile, outfile):
     ofile = open(outfile,'w')
     retcode = subprocess.call(["mafft",infile], stdout=ofile)
     ofile.close()
     if retcode != 0:
-        raise Exception("Possible error in MAFFT alignment")    
-        
+        raise Exception("Possible error in MAFFT alignment")
+
 def hmmer_pfam(infilename, outfilename, pfamdb):
-    pipe = subprocess.Popen(["hmmscan", pfamdb, infilename], 
+    pipe = subprocess.Popen(["hmmscan", pfamdb, infilename],
             stdout=subprocess.PIPE).stdout
     output = pipe.read() # this gives us the output of our command
     outfile = open(outfilename, 'w')
     outfile.write(output)
     outfile.close()
-    
+
 def oneseq_pipeline(infilename, pfamdb=None,
                     compareto=['Homo sapiens','Drosophila melanogaster'],
                     skipHMMER = True,extension="XX"):
     # translate nucleotide sequence to protein seq
-    protout = 'protein-' + infilename + extension     
+    protout = 'protein-' + infilename + extension
                 # add the extension so all generated files have
                 # different extension than input files
-                
+
     translate_fasta(infilename, protout)
-    
+
     # run blastp on protein sequence against swissprot and extract best hits
-    protrec = parse_fasta(protout)[0]        
-    blastout ='blast-' + protout + '.XML'        
+    protrec = parse_fasta(protout)[0]
+    blastout ='blast-' + protout + '.XML'
     besthitids = []
     for organism in compareto:
         equery = '(%s[ORGN])' % organism # create the entrez organism query
@@ -119,26 +118,26 @@ def oneseq_pipeline(infilename, pfamdb=None,
     # download corresponding records from Swiss-Prot
     swissrecs = [get_swissrec(i) for i in besthitids]
     seqs = [swissrec2seqrec(i) for i in swissrecs]
-    seqs.append(protrec)    
-    
+    seqs.append(protrec)
+
     # write Fasta file with best hits plus original protein sequence
     plusout = 'blasthits-' + protout
-    write_fasta(seqs, plusout)     
-    
+    write_fasta(seqs, plusout)
+
     # do multiple alignment via mafft
     mafft_align(plusout, 'aligned-' + protout)
 
-    # search for domains via HMMER/Pfam     
-    if not skipHMMER:  
-        if pfamdb is not None: 
+    # search for domains via HMMER/Pfam
+    if not skipHMMER:
+        if pfamdb is not None:
             hmmerout = 'hmmer-' + protout
-            hmmer_pfam(protout, hmmerout, pfamdb)         
-                        
-def multiseq_pipeline(inext, pfamdb=None, 
+            hmmer_pfam(protout, hmmerout, pfamdb)
+
+def multiseq_pipeline(inext, pfamdb=None,
                 compareto=['Homo sapiens','Drosophila melanogaster'],
-                skipHMMER=True):   
+                skipHMMER=True):
     inout = inout_pairs(inext, 'XX')
     infiles = [i[0] for i in inout]
     for filename in infiles:
         print "Processing %s" % filename
-        oneseq_pipeline(filename, pfamdb, compareto, skipHMMER)                          
+        oneseq_pipeline(filename, pfamdb, compareto, skipHMMER)
